10bb20a5aba8f6cb432e4ba960933e379b690bab
njwalker <njwalker@5a2a75f4-3e5b-4b7d-bb5a-54c266e05728>
2001-10-29 15:52:52 +0000

    Simple test!

--- merlin_config.h
+++ merlin_config.h
@@ -1,6 +1,7 @@
 // merlin_config.h
 // configuration and platform/compiler dependent information
 // Currently only needed for MSVC++
+// Need to update this file for GCC 3.1
 
 #ifndef _h_merlin_config
 #define _h_merlin_config
