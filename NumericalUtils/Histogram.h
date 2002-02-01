a54cf3c20ff9ae82abdaf412c9661e99ab55ae18
njwalker <njwalker@5a2a75f4-3e5b-4b7d-bb5a-54c266e05728>
2002-02-01 12:55:51 +0000

    Added routines to support histogramming.

---
+++ NumericalUtils/Histogram.h
@@ -0,0 +1,9 @@
+#ifndef _h_Histogram
+#define _h_Histogram 1
+
+#include <vector>
+
+size_t Hist(const std::vector<double>& data, double x1, double x2, double dx, std::vector<double>& hist);
+
+
+#endif
