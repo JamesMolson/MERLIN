af295fddce1849ffc7604d2bef2679facc9e53b7
njwalker <njwalker@5a2a75f4-3e5b-4b7d-bb5a-54c266e05728>
2004-12-10 17:27:54 +0000

    THIS IS A TEST!

--- AcceleratorModel/WakePotentials.cpp
+++ AcceleratorModel/WakePotentials.cpp
@@ -1,30 +1,17 @@
-/*
- * Merlin C++ Class Library for Charged Particle Accelerator Simulations
- * 
- * Class library version 2.0 (1999)
- * 
- * file Merlin\AcceleratorModel\WakePotentials.h
- * last modified 06/02/03
- */
-
-/*
- * This file is derived from software bearing the following
- * restrictions:
- *
- * MERLIN C++ class library for 
- * Charge Particle Accelerator Simulations
- * Copyright (c) 2001 by The Merlin Collaboration.
- * - ALL RIGHTS RESERVED - 
- *
- * Permission to use, copy, modify, distribute and sell this
- * software and its documentation for any purpose is hereby
- * granted without fee, provided that the above copyright notice
- * appear in all copies and that both that copyright notice and
- * this permission notice appear in supporting documentation.
- * No representations about the suitability of this software for
- * any purpose is made. It is provided "as is" without express
- * or implied warranty.
- */
+/////////////////////////////////////////////////////////////////////////
+//
+// Merlin C++ Class Library for Charged Particle Accelerator Simulations
+//  
+// Class library version 3 (2004)
+// 
+// Copyright: see Merlin/copyright.txt
+//
+//
+// $Date: 2004-12-10 17:27:54 $
+// $Revision: 1.2 $
+// $Author: njwalker $
+// 
+/////////////////////////////////////////////////////////////////////////
 
 #include "WakePotentials.h"
 
