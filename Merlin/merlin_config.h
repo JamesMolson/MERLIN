19bca148301e24bf47cb16391885ffaefe937304
James Molson <molson@lal.in2p3.fr>
2016-01-20 10:43:52 +0100

    Add more fp and vector types.

--- Merlin/merlin_config.h
+++ Merlin/merlin_config.h
@@ -39,11 +39,45 @@
 #define _MAX(a,b) std::max(a,b)
 #define _MIN(a,b) std::min(a,b)
 
-//Define the floating point type used in Merlin
-/** Use doubles (64-bit) */
-typedef double Mfloat_t;
+/**
+* Floating point and vector definitions
+* Do not mix global single precision floats with vectors that are expecting doubles.
+* Define the floating point type, and the vector type used in Merlin
+*/
+
+/**
+* Single precision variables (32-bit math)
+*/
 
 /** Use floats (32-bit) */
 //typedef float Mfloat_t;
+
+/** Use 128bit (SSE2) length vectors - aka 4 singles */
+//typedef __m128 Mvector_t;
+
+/** Use 256bit (AVX) length vectors - aka 8 singles */
+//typedef __m256 Mvector_t;
+
+/** Use 512bit (AVX-512) length vectors - aka 16 singles */
+//typedef __m512 Mvector_t;
+
+/**
+* Double precision variables (64-bit math)
+*/
+
+/** Use doubles (64-bit) */
+typedef double Mfloat_t;
+
+/** Use 128bit (SSE2) length vectors - aka 2 doubles */
+//typedef __m128d Mvector_t;
+
+/** Use 256bit (AVX) length vectors - aka 4 doubles */
+typedef __m256d Mvector_t;
+
+/** Use 512bit (AVX-512) length vectors - aka 8 doubles */
+//typedef __m512d Mvector_t;
+
+/** Fixed global variable to define the number of float types in a vector type. */
+static const unsigned int MvectorLength = sizeof(Mvector_t)/sizeof(Mfloat_t);
 
 #endif /* _h_merlin_config */
